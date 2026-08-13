fdf5e95f1344f54e35503214be6de5d8ca396d84
synchronierer <153615360+synchronierer@users.noreply.github.com>
2026-08-13 22:46:31 +0200

    Implement AttendancePlugin with lifecycle methods

--- src/main/java/de/igslandstuhl/database/attendance/AttendancePlugin.java
+++ src/main/java/de/igslandstuhl/database/attendance/AttendancePlugin.java
@@ -1,1 +1,30 @@
-// placeholder
+package de.igslandstuhl.database.attendance;
+
+import de.igslandstuhl.database.plugins.Plugin;
+
+public class AttendancePlugin extends Plugin {
+
+    private AttendancePluginConfig config;
+
+    @Override
+    protected void onLoad() {
+        config = new AttendancePluginConfig(this);
+        AttendanceRequests.register();
+        getLogger().info("Attendance plugin loaded");
+    }
+
+    @Override
+    protected void onEnable() {
+        getLogger().info("Attendance plugin enabled");
+    }
+
+    @Override
+    protected void onDisable() {
+        getLogger().info("Attendance plugin disabled");
+    }
+
+    @Override
+    public AttendancePluginConfig getConfig() {
+        return config;
+    }
+}
